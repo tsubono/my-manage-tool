b960691a38f559d20a18a848e2feb5a77f618aa7
tsubono <tsubono@wss.systems>
2020-05-13 18:13:38 +0900

    2階層のページでもサイドバーがactiveになるように修正

--- client/components/common/Sidebar.vue
+++ client/components/common/Sidebar.vue
@@ -4,33 +4,39 @@
       <i class="ti-panel"></i>
       <p>Top</p>
     </sidebar-link>
-    <sidebar-link to="/clients">
+    <sidebar-link
+      to="/clients"
+      :class="{ 'active': $nuxt.$route.name.match(/clients/) }">
       <i class="ti-face-smile"></i>
       <p>取引先一覧</p>
     </sidebar-link>
-    <sidebar-link to="/projects">
+    <sidebar-link
+      to="/projects"
+      :class="{ 'active': $nuxt.$route.name.match(/projects/) }">
       <i class="ti-crown"></i>
       <p>案件一覧</p>
     </sidebar-link>
-    <sidebar-link to="/todos">
+    <sidebar-link
+      to="/todos"
+      :class="{ 'active': $nuxt.$route.name.match(/todos/) }">
       <i class="ti-arrow-circle-down"></i>
       <p>TODO</p>
     </sidebar-link>
-    <sidebar-link to="/memos">
+    <sidebar-link
+      to="/memos"
+      :class="{ 'active': $nuxt.$route.name.match(/memos/) }">
       <i class="ti-pencil"></i>
       <p>メモ</p>
     </sidebar-link>
     <sidebar-link
       :to="{ name:'sales-year-month', params: { year: new Date().getFullYear(), month: new Date().getMonth() + 1 } }"
-      :class="{'active': $nuxt.$route.name === 'sales-year-month' }"
-    >
+      :class="{ 'active': $nuxt.$route.name === 'sales-year-month' }">
       <i class="ti-medall"></i>
       <p>売上</p>
     </sidebar-link>
     <sidebar-link
       :to="{ name:'sales-year-analysis', params: { year: new Date().getFullYear() } }"
-      :class="{'active': $nuxt.$route.name === 'sales-year-analysis' }"
-    >
+      :class="{ 'active': $nuxt.$route.name === 'sales-year-analysis' }">
       <i class="ti-bar-chart"></i>
       <p>分析</p>
     </sidebar-link>

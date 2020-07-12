34a4a96f38c2563b4ac050d9f289bb5cdf51285d
kim <>
2020-07-12 18:18:03 +0900

    デプロイ実行前に.bash_profileを読み直す

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,6 +10,8 @@ sudo chown manage:manage -R /home/manage/my-manage-tool/
 cd /home/manage/my-manage-tool/
 ln -nfs storage /home/manage/config/storage
 sudo chmod -R 777 storage
+cd /home/manage/my-manage-tool/
+pwd
 composer install --optimize-autoloader --no-dev
 php artisan config:cache
 php artisan migrate --force

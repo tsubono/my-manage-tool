1ab3eb7c924cd9c14914f7f9955334736e86e257
kim <>
2020-07-23 13:49:33 +0900

    b/gデプロイ追加に伴う終了処理の省略

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo pkill -KILL -f nuxt
-sudo service httpd stop
-cd /home/manage/my-manage-tool
-sudo rm -r storage
-cd
-sudo rm -r /home/manage/my-manage-tool
+#sudo pkill -KILL -f nuxt
+#sudo service httpd stop
+#cd /home/manage/my-manage-tool
+#sudo rm -r storage
+#cd
+#sudo rm -r /home/manage/my-manage-tool
